15303d6000c323b5c84b6a4ed84a4ac28c20fbb7
전승원 <jswa7308@naver.com>
2022-05-01 22:36:16 +0900

    :tada: :: project start

---
+++ src/test/java/com/example/msgadminapi/MsgAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
