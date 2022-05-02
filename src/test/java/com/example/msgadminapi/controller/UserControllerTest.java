29c087f8abf4cbed3eef908d2f7ac6a5f8ccdc8b
전승원 <jswa7308@naver.com>
2022-05-02 16:10:31 +0900

    :sparkles: :: Controller 잘 작동되나 테스트

---
+++ src/test/java/com/example/msgadminapi/controller/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.msgadminapi.controller;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("findAllMember")
+    public void 전체회원조회() throws Exception {
+        String data = "전체 회원";
+
+        mvc.perform(get("/user"))
+                .andExpect(status().isOk());
+    }
+
+}
