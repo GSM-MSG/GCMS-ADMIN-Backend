61914fdd71d66f012b6fbb153c1b9ae1fcd5b44c
전승원 <jswa7308@naver.com>
2022-05-03 21:09:45 +0900

    :sparkles: :: 이메일을 통한 회원 조회JPA

--- src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
+++ src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.msgadminapi.domain.repository;
 import com.example.msgadminapi.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
 
 }
