bde0f2d89639af37d8266f63c07180ee4c08e278
전승원 <jswa7308@naver.com>
2022-05-03 21:06:48 +0900

    :sparkles: :: 멤버정보수정 Controller 제작

--- src/main/java/com/example/msgadminapi/controller/UserController.java
+++ src/main/java/com/example/msgadminapi/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.msgadminapi.controller;
 
+import com.example.msgadminapi.dto.request.UserRequestDto;
 import com.example.msgadminapi.dto.response.UserResponseDto;
+import com.example.msgadminapi.response.ResponseService;
+import com.example.msgadminapi.response.result.CommonResultResponse;
 import com.example.msgadminapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +16,21 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final ResponseService responseService;
 
     @GetMapping("/users")
     public List<UserResponseDto> userFindAll() {
         return userService.findAll();
     }
 
+    @PutMapping("/modify")
+    public CommonResultResponse userModify(@RequestBody UserRequestDto userRequestDto) {
+        userService.userModify(userRequestDto);
+        return responseService.getSuccessResult();
+    }
+
+
+
+
 }
+
