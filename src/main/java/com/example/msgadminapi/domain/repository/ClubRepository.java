647bd704d71fc0237c720031817ef696dd481705
전승원 <jswa7308@naver.com>
2022-05-10 22:44:48 +0900

    :sparkles: :: Repository 어노테이션 추가

--- src/main/java/com/example/msgadminapi/domain/repository/ClubRepository.java
+++ src/main/java/com/example/msgadminapi/domain/repository/ClubRepository.java
@@ -3,9 +3,12 @@ package com.example.msgadminapi.domain.repository;
 import com.example.msgadminapi.domain.entity.club.Club;
 import com.example.msgadminapi.dto.response.ClubResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
 
     List<ClubResponseDto> findByTitleContaining(String title);
--- src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
+++ src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.msgadminapi.domain.repository;
 
 import com.example.msgadminapi.domain.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
 
