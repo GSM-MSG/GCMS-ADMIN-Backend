be98c963b726c87272f661bf320ff2fb753e350f
전승원 <jswa7308@naver.com>
2022-05-10 22:44:15 +0900

    :sparkles: :: isOpened columnType TinyInt로 변경

--- src/main/java/com/example/msgadminapi/domain/entity/club/Club.java
+++ src/main/java/com/example/msgadminapi/domain/entity/club/Club.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Optional;
 
 @Getter
@@ -31,6 +28,7 @@ public class Club {
 
     private String teacher;
 
+    @Column(columnDefinition = "TINYINT")
     private boolean isOpened;
 
 
