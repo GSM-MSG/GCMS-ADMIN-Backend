2781ba03298623a533145916eed0662500f1e676
전승원 <jswa7308@naver.com>
2022-05-24 14:15:39 +0900

    :ambulance: :: 불필요한 GeneratedValue 삭제

--- src/main/java/com/example/msgadminapi/domain/entity/user/User.java
+++ src/main/java/com/example/msgadminapi/domain/entity/user/User.java
@@ -17,7 +17,6 @@ import java.util.List;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
     private String email;
 
     private String name;
--- src/main/java/com/example/msgadminapi/domain/repository/MemberRepository.java
+++ src/main/java/com/example/msgadminapi/domain/repository/MemberRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    
+
 }
