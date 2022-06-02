223d0cd7647f8969dd71c92081f27a839c1b6333
전승원 <jswa7308@naver.com>
2022-06-02 20:23:51 +0900

    :sparkles: :: 동아리 구성원 예외처리

--- src/main/java/com/example/msgadminapi/service/MemberService.java
+++ src/main/java/com/example/msgadminapi/service/MemberService.java
@@ -7,6 +7,10 @@ import com.example.msgadminapi.domain.entity.user.User;
 import com.example.msgadminapi.domain.repository.ClubRepository;
 import com.example.msgadminapi.domain.repository.MemberRepository;
 import com.example.msgadminapi.domain.repository.UserRepository;
+import com.example.msgadminapi.exception.ErrorCode;
+import com.example.msgadminapi.exception.exception.ClubNotFindException;
+import com.example.msgadminapi.exception.exception.MemberNotFindException;
+import com.example.msgadminapi.exception.exception.UserNotFindException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,27 +29,27 @@ public class MemberService {
     private final ClubRepository clubRepository;
 
     public void insertMember(String email, Long clubIdx) {
-        System.out.println("club email" + email);
         Member member = new Member();
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("오류1"));
-        System.out.println("1" + user);
-        Club club = clubRepository.findById(clubIdx).orElseThrow(()-> new RuntimeException("오류"));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFindException("동아리 구성원을 강제 추가하려는 과정중에 유저를 찾지 못했습니다.", ErrorCode.USER_NOT_FIND));
+        Club club = clubRepository.findById(clubIdx)
+                .orElseThrow(()-> new ClubNotFindException("동아리 구성원을 강제 추가하려는 과정중에 동아리를 찾지 못했습니다.", ErrorCode.CLUB_NOT_FIND));
         member.userMapping(user);
         member.clubMapping(club);
         memberRepository.save(member);
     }
 
-    public void deleteMember(Long id) throws Exception{
+    public void deleteMember(Long id) {
         Member member = memberRepository.findById(id)
-                        .orElseThrow(() -> new Exception("존재하지 않는 멤버 엔티티 튜플입니다."));
+                        .orElseThrow(() -> new MemberNotFindException("동아리 구성원을 강제로 지우는 과정중에 구성원을 찾지 못했습니다.", ErrorCode.MEMBER_NOT_FIND));
         memberRepository.delete(member);
     }
 
     public void moveMember(Long memberIdx, Long clubIdx) {
         Member member = memberRepository.findById(memberIdx)
-                .orElseThrow(() -> new RuntimeException("런타임 오류"));
+                .orElseThrow(() -> new MemberNotFindException("동아리 구성원을 강제로 이동하는 과정중에 구성원을 찾지 못했습니다.", ErrorCode.MEMBER_NOT_FIND));
         Club club = clubRepository.findById(clubIdx)
-                .orElseThrow(() -> new RuntimeException("런타임 오류2"));
+                .orElseThrow(() -> new ClubNotFindException("동아리 구성원을 강제로 이동하는 과정중에 동아리를 찾지 못했습니다.", ErrorCode.CLUB_NOT_FIND));
         member.getClub().getMembers().remove(member);
         member.clubMapping(club);
     }
@@ -53,7 +57,7 @@ public class MemberService {
     @Transactional
     public void changeManager(Long memberIdx) {
         Member member = memberRepository.findById(memberIdx)
-                .orElseThrow(() -> new RuntimeException("멤버 Change 함수에서 불러오다 오류"));
+                .orElseThrow(() -> new MemberNotFindException("동아리 부장을 교체하려는 과정중에 구성원을 찾지 못했습니다.", ErrorCode.MEMBER_NOT_FIND));
         Set<Member> members = member.getClub().getMembers();
         for(Member m : members) {
             if(m.getScope() == Scope.HEAD) {
