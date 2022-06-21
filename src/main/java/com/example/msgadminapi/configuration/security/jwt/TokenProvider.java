900d145ed51d6c2e769eb91eb076efcdc369dd3b
전승원 <jswa7308@naver.com>
2022-06-21 23:00:18 +0900

    :sparkles: :: Secret Key랑 enum 추가

---
+++ src/main/java/com/example/msgadminapi/configuration/security/jwt/TokenProvider.java
@@ -0,0 +1,36 @@
+package com.example.msgadminapi.configuration.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 3; // 3시간
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = ACCESS_TOKEN_EXPIRE_TIME * 8 * 180;
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    enum TokenType {
+        ACCESS_TOKEN("accessToken"),
+        REFRESH_TOKEN("refreshToken");
+        String value;
+        TokenType(String value) {
+            this.value = value;
+        }
+    }
+    enum TokenClaimName {
+        USER_EMAIL("userEmail"),
+        TOKEN_TYPE("tokenType");
+        String value;
+        TokenClaimName(String value) {
+            this.value = value;
+        }
+    }
