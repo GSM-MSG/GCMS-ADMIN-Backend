d0e68e339573be4782c75e6e676a3bf05b1a964a
전승원 <jswa7308@naver.com>
2022-06-21 23:03:41 +0900

    :sparkles: :: User 에서 UserDetails 구현

--- src/main/java/com/example/msgadminapi/domain/entity/user/User.java
+++ src/main/java/com/example/msgadminapi/domain/entity/user/User.java
@@ -5,8 +5,11 @@ import com.example.msgadminapi.domain.entity.member.Member;
 import com.example.msgadminapi.domain.entity.requestjoin.RequestJoin;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,7 +20,7 @@ import java.util.Set;
 @Builder
 @ToString
 @Table(name = "users") // 나중에 본 DB 연결 할때 지워야됨
-public class User {
+public class User implements UserDetails {
     @Id
     private String email;
 
@@ -50,5 +53,44 @@ public class User {
         this.grade = grade;
         this.class_ = class_;
         this.num = num;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
