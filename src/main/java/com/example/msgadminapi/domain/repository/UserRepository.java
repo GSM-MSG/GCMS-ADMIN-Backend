0f81ba84590acf7518492b2ce047e480a4e5c610
전승원 <jswa7308@naver.com>
2022-06-21 23:04:40 +0900

    :sparkles: :: findOneByEmail 로 유저 찾기

--- src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
+++ src/main/java/com/example/msgadminapi/domain/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.msgadminapi.domain.repository;
 
 import com.example.msgadminapi.domain.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 
     public List<User> findAllByNameLike(String keyword);
+
+     User findOneByEmail(String email);
 }
