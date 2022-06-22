d51424d4069b5d079cee82a4e4d9c9ce12911f36
전승원 <jswa7308@naver.com>
2022-06-22 14:08:31 +0900

    :ambulance: UserDetails 구현체를 따로 MyUserDetails로 뺌

---
+++ src/main/java/com/example/msgadminapi/configuration/security/auth/MyUserDetails.java
@@ -0,0 +1,49 @@
+package com.example.msgadminapi.configuration.security.auth;
+
+import com.example.msgadminapi.domain.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class MyUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
